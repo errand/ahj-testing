4381a01f62626674f776eb94b2dde98454091ae4
Aleksandr Shatskikh <work@errand.ru>
2022-01-27 19:02:41 +0300

    tests: fail

--- src/js/__tests__/CardsValidator.test.js
+++ src/js/__tests__/CardsValidator.test.js
@@ -4,38 +4,23 @@
 
 import CardsValidator from '../CardsValidator';
 
+const validator = new CardsValidator();
+
 /* eslint-disable quotes */
 test('Validate Luhn Algorithm', () => {
-  const validator = new CardsValidator();
   expect(validator.luhnAlgorithm('6011870328613948')).toBeTruthy();
 });
 
 test('Fail Luhn Algorithm for Cards', () => {
-  const validator = new CardsValidator();
   expect(validator.luhnAlgorithm('3434')).not.toBeTruthy();
 });
 
-test('Validate MASTERCARD card', () => {
-  const validator = new CardsValidator();
-  expect(validator.paymentSystemCheck('5318065797202099')).toBe('mastercard');
-});
-
-test('Validate VISA card', () => {
-  const validator = new CardsValidator();
-  expect(validator.paymentSystemCheck('4758024707169070')).toBe('visa');
-});
-
-test('Validate AMEX card', () => {
-  const validator = new CardsValidator();
-  expect(validator.paymentSystemCheck('3013522570371072')).toBe('amex');
-});
-
-test('Validate MIR card', () => {
-  const validator = new CardsValidator();
-  expect(validator.paymentSystemCheck('2200972321544391')).toBe('mir');
-});
-
-test('Validate Discover card', () => {
-  const validator = new CardsValidator();
-  expect(validator.paymentSystemCheck('6011870328613948')).toBe('discover');
+test.each([
+  ['5318065797202099', 'mastercard'],
+  ['4758024707169070', 'visa'],
+  ['3013522570371072', 'amex'],
+  ['2200972321544391', 'mir'],
+  ['6011870328613948', 'discover'],
+])('System is %expected', (num, expected) => {
+  expect(validator.paymentSystemCheck(num)).toBe(expected);
 });
