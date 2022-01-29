fe5560ec3f58cb7432aece9a0377efb789b014b5
Aleksandr Shatskikh <work@errand.ru>
2022-01-29 14:35:38 +0300

    test appveyor check

--- src/js/__tests__/dom.test.js
+++ src/js/__tests__/dom.test.js
@@ -9,9 +9,9 @@ describe('Credit Card Validator form', () => {
 
   beforeAll(async () => {
     browser = await puppetteer.launch({
-      // headless: false, // show gui
-      // slowMo: 250,
-      // devtools: true, // show devTools
+      headless: false, // show gui
+      slowMo: 250,
+      devtools: true, // show devTools
     });
     page = await browser.newPage();
   });
