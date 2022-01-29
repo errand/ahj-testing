fa005ac84bd575d00e42da360091bac286c741b2
Aleksandr Shatskikh <work@errand.ru>
2022-01-29 14:45:27 +0300

    refactor: testing

--- src/js/e2e/e2e.test.js
+++ src/js/e2e/e2e.test.js
@@ -1,17 +1,19 @@
 import puppetteer from 'puppeteer';
+import { fork } from 'child_process';
 
 jest.setTimeout(30000); // default puppeteer timeout
 
 describe('Credit Card Validator form', () => {
   let browser = null;
   let page = null;
+  const server = null;
   const baseUrl = 'http://localhost:9000';
 
   beforeAll(async () => {
     browser = await puppetteer.launch({
-      headless: false, // show gui
-      slowMo: 250,
-      devtools: true, // show devTools
+      // headless: false, // show gui
+      // slowMo: 250,
+      // devtools: true, // show devTools
     });
     page = await browser.newPage();
   });
